16a355f495df2e4571f3c89a19ba3b5e59f83492
Alex Gushin <a.gushin@terabit.ai>
2025-07-22 19:45:47 +0300

    Updates

--- _nuxt/static/1753202510/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-test\u002F_nuxt\u002Fstatic\u002F1753202510",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-test\u002F",assetsPath:"\u002Fnuxt-test\u002F_nuxt\u002F",cdnURL:a}}}}(null));
